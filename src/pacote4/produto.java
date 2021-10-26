3e762968a58cd2b30ca481eb81125fd64c455424
João Pedro  Bandeira <joao.bandeiraoliveira@gmail.com>
2021-10-26 00:23:04 -0300

    Add files via upload

---
+++ src/pacote4/produto.java
@@ -0,0 +1,37 @@
+package pacote4;
+
+public class produto {
+    private String nome;
+    private int codigo;
+    private float preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "produto: " + nome + "\ncodigo: " + codigo + "\npreço: R$" + preco + "";
+    }
+
+}
